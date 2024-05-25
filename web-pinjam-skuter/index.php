8150645ddfa69191e54790b7e2cf32f72e315ec6
Ghazy Aulia Wicaksana <137251485+Ghazy15@users.noreply.github.com>
2024-05-25 15:00:48 +0700

    Mengisi kode index.php

--- web-pinjam-skuter/index.php
+++ web-pinjam-skuter/index.php
@@ -1,1 +1,25 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Selamat Datang</title>
+</head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="login.php">Login</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main style="text-align: center;">
+            <h1>Selamat Datang di Website Peminjaman Skuter</h1>
+            <p>Silahkan login terlebih dulu</p>
+        </main>    
+        <footer style="background: #4161a1; text-align: center; padding: 5px;">
+            <p>&copy; 2024 Web Peminjaman Skuter</p>
+        </footer>
+    </body>
+</html>
