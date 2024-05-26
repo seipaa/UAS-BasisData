7dd9b5612eeae9d842834e80038d013d9dca7b81
Runni1 <148168270+Runni1@users.noreply.github.com>
2024-05-26 13:45:26 +0700

    Membuat file dashboard.php

--- web-pinjam-skuter/folder operator/dashboard.php
+++ web-pinjam-skuter/folder operator/dashboard.php
@@ -1,1 +1,6 @@
-
+<?php include '../includes/header_operator.php'; ?>
+<main>
+    <h1>Dashboard Operator</h1>
+    <h3>Halo, <?php echo $_SESSION['username']; ?></h3>
+</main>    
+<?php include '../includes/footer.php'; ?>
